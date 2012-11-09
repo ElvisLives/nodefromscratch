1a6cad40370a6d824b9d92f632faa0620181b9b4
ElvisLives <code@ps.otherinbox.com>
2012-11-09 08:43:06 -0700

    Added the basic routing and requesthandlers via node modules.

---
+++ router.js
@@ -0,0 +1,11 @@
+function route(handle, pathname){
+	console.log("About to route a request for " + pathname);
+
+	if (typeof handle[pathname] === 'function') {
+		handle[pathname](); // call the handler
+	} else {
+		console.log("No request handler found for " + pathname);
+	}
+}
+
+exports.route = route;
