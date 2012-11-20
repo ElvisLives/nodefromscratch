8e8f9f9346a022b6980b48da8fbcbbc07a545a84
ElvisLives <code@ps.otherinbox.com>
2012-11-20 09:23:47 -0700

    Added file upload and show

--- router.js
+++ router.js
@@ -1,12 +1,11 @@
-function route(handle, pathname,response, postData){
+function route(handle, pathname, response, request) {
 	console.log("About to route a request for " + pathname);
-
 	if (typeof handle[pathname] === 'function') {
-		handle[pathname](response, postData); // call the handler
+		handle[pathname](response, request);
 	} else {
 		console.log("No request handler found for " + pathname);
-		response.writeHead(404, {"Content-Type":"text/plain"});
-		response.write("404 Not Found");
+		response.writeHead(404, {"Content-Type": "text/html"});
+		response.write("404 Not found");
 		response.end();
 	}
 }
