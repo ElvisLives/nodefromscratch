75520a03a9154c2fe4c5da16020434472c1339f7
ElvisLives <code@ps.otherinbox.com>
2012-11-20 09:04:43 -0700

    Updated to handle post from form

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-function route(handle, pathname,response){
+function route(handle, pathname,response, postData){
 	console.log("About to route a request for " + pathname);
 
 	if (typeof handle[pathname] === 'function') {
-		handle[pathname](response); // call the handler
+		handle[pathname](response, postData); // call the handler
 	} else {
 		console.log("No request handler found for " + pathname);
 		response.writeHead(404, {"Content-Type":"text/plain"});
